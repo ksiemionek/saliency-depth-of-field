a403fe45296231cc8fc3c26f956a9a11acfc5f26
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-04-21 17:35:00 +0200

    Refactor saliency map generation in object-oriented

--- object-oriented-pretrained/gen_saliency.py
+++ object-oriented-pretrained/gen_saliency.py
@@ -2,7 +2,17 @@ from transparent_background import Remover
 from PIL import Image
 
 
-remover = Remover()
-img = Image.open("./original.jpeg").convert('RGB')
-saliency_map = remover.process(img, type='map')
-saliency_map.save("./saliency.png")
+def generate_saliency(image_path):
+    remover = Remover()
+    img = Image.open(image_path).convert('RGB')
+    saliency_map = remover.process(img, type='map')
+
+    return saliency_map
+
+
+if __name__ == "__main__":
+    saliency_map = generate_saliency(
+        image_path="./../images/original.jpeg",
+    )
+
+    saliency_map.save("./result/saliency_object_oriented.png")
