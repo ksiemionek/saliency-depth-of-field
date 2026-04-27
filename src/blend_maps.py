b40405e8f71b0555841eedf6bb23b8ba4145e509
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-04-27 13:03:42 +0200

    Modify blend maps calculations

--- src/blend_maps.py
+++ src/blend_maps.py
@@ -4,7 +4,7 @@ def blend_maps(depth, saliency):
     depth = depth.astype(np.float32) / 255.0
     saliency = saliency.astype(np.float32) / 255.0
 
-    blur_mask = 1.0 - (1.0 - depth) * (1.0 - saliency)
+    blur_mask = np.clip(depth + saliency, 0.0, 1.0)
     blur_mask = (blur_mask * 255).astype(np.uint8)
     
     return blur_mask
