4de0e12642511ebc62d7cf2009ca8b0b08837ff7
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-05-20 13:57:04 +0200

    Refactor DUT-OMRON and SALICON dataset classes to use config

--- src/saliency_net/data/dataset.py
+++ src/saliency_net/data/dataset.py
@@ -4,17 +4,18 @@ from PIL import Image
 from pathlib import Path
 import random
 import torchvision.transforms.functional as TF
+import config
 
 
 class DUTOMRONDataset(Dataset):
     def __init__(self, img_transform=None, heatmap_transform=None):
-        self.root_dir = Path('./data/DUT-OMRON')
+        self.root_dir = Path(config.DUT_OMRON_DIR)
         self.img_transform = img_transform
         self.heatmap_transform = heatmap_transform
 
-        self.img_dir = self.root_dir / 'DUT-OMRON-image'
-        self.img_list = sorted(self.img_dir.glob('*.jpg'))
-        self.heatmap_dir = self.root_dir / 'DUT-OMRON-heatmaps'
+        self.img_dir = self.root_dir / "DUT-OMRON-image"
+        self.img_list = sorted(self.img_dir.glob("*.jpg"))
+        self.heatmap_dir = self.root_dir / "DUT-OMRON-heatmaps"
 
     def __len__(self):
         return len(self.img_list)
@@ -22,33 +23,33 @@ class DUTOMRONDataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
-        img_path = self.img_list[idx]
-        heatmap_path = self.heatmap_dir / img_path.with_suffix('.png').name
 
-        img = Image.open(img_path).convert('RGB')
-        heatmap = Image.open(heatmap_path).convert('L')
-        
-        sample = {'image': img, 'heatmap': heatmap}
+        img_path = self.img_list[idx]
+        heatmap_path = self.heatmap_dir / img_path.with_suffix(".png").name
+
+        img = Image.open(img_path).convert("RGB")
+        heatmap = Image.open(heatmap_path).convert("L")
+
+        sample = {"image": img, "heatmap": heatmap}
 
         if self.img_transform:
-            sample['image'] = self.img_transform(sample['image'])
+            sample["image"] = self.img_transform(sample["image"])
         if self.heatmap_transform:
-            sample['heatmap'] = self.heatmap_transform(sample['heatmap'])
-        
+            sample["heatmap"] = self.heatmap_transform(sample["heatmap"])
+
         return sample
 
 
 class SALICONDataset(Dataset):
     def __init__(self, split, img_transform=None, heatmap_transform=None):
-        self.root_dir = Path('./data/SALICON')
+        self.root_dir = Path(config.SALICON_DIR)
         self.split = split
         self.img_transform = img_transform
         self.heatmap_transform = heatmap_transform
 
-        self.img_dir = self.root_dir / 'images' / self.split
-        self.img_list = sorted(self.img_dir.glob('*.jpg'))
-        self.heatmap_dir = self.root_dir / 'maps' / self.split
+        self.img_dir = self.root_dir / "images" / self.split
+        self.img_list = sorted(self.img_dir.glob("*.jpg"))
+        self.heatmap_dir = self.root_dir / "maps" / self.split
 
     def __len__(self):
         return len(self.img_list)
@@ -56,26 +57,26 @@ class SALICONDataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
+
         img_path = self.img_list[idx]
-        img = Image.open(img_path).convert('RGB')
+        img = Image.open(img_path).convert("RGB")
 
         heatmap = None
-        if self.split in ['train', 'val']:
-            heatmap_path = self.heatmap_dir / img_path.with_suffix('.png').name
-            heatmap = Image.open(heatmap_path).convert('L')
-        
-        if self.split == 'train' and random.random() < 0.5:
+        if self.split in ["train", "val"]:
+            heatmap_path = self.heatmap_dir / img_path.with_suffix(".png").name
+            heatmap = Image.open(heatmap_path).convert("L")
+
+        if self.split == "train" and random.random() < 0.5:
             img = TF.hflip(img)
             heatmap = TF.hflip(heatmap)
-        
-        sample = {'image': img}
+
+        sample = {"image": img}
         if heatmap is not None:
-            sample['heatmap'] = heatmap
+            sample["heatmap"] = heatmap
 
         if self.img_transform:
-            sample['image'] = self.img_transform(sample['image'])
+            sample["image"] = self.img_transform(sample["image"])
         if self.heatmap_transform:
-            sample['heatmap'] = self.heatmap_transform(sample['heatmap'])
-        
+            sample["heatmap"] = self.heatmap_transform(sample["heatmap"])
+
         return sample
